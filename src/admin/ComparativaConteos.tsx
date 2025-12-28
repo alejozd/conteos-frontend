24332d502ce45a7aa28cfb21a933b1fb1637d493
alejozd <alejozd79@gmail.com>
2025-12-28 13:39:44 -0500

    correccion sobra react

--- src/admin/ComparativaConteos.tsx
+++ src/admin/ComparativaConteos.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { MultiSelect } from "primereact/multiselect";
