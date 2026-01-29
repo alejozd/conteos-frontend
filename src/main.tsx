333ce1ce3c8a12af7637e82269fd96a9f0ca7006
Alejandro Zambrano <alejozd79@gmail.com>
2026-01-29 13:57:44 -0500

    Mejora para mensaje en español

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 // src/main.tsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { PrimeReactProvider, addLocale } from "primereact/api";
 
 // Temas de PrimeReact (orden importante)
 // import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import 'primereact/resources/themes/lara-dark-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import "primereact/resources/themes/lara-dark-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 addLocale("es", {
   accept: "Sí",
@@ -20,12 +20,17 @@ addLocale("es", {
   cancel: "Cancelar",
   pending: "Pendiente",
   emptyMessage: "No hay archivos",
+  emptyFilterMessage: "No hay resultados",
+  weak: "Débil",
+  medium: "Medio",
+  strong: "Fuerte",
+  passwordPrompt: "Ingrese una contraseña",
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-     <PrimeReactProvider value={{ locale: "es" }}>
+    <PrimeReactProvider value={{ locale: "es" }}>
       <App />
     </PrimeReactProvider>
   </React.StrictMode>,
-)
+);
